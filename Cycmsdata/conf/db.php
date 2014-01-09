9b7b584083b64d2b60496d838a0ff78eb0b6eb38
Ren <renlincn@gmail.com>
2014-01-09 11:17:03 +0800

    安装

--- Cycmsdata/conf/db.php
+++ Cycmsdata/conf/db.php
@@ -3,9 +3,9 @@ return array (
   'DB_TYPE' => 'mysql',
   'DB_HOST' => 'localhost',
   'DB_PORT' => '3306',
-  'DB_NAME' => 'shopwx',
-  'DB_USER' => 'shopwx',
-  'DB_PWD' => 'shopwx',
+  'DB_NAME' => 'web_cms',
+  'DB_USER' => 'root',
+  'DB_PWD' => '2468',
   'DB_PREFIX' => 'Cycms_',
 );
 ?>
